b185152a073d454538019bbdc2e32c93ff2eafb0
danieljpay <danielj.pay@hotmail.com>
2022-04-05 22:34:47 -0500

    Moved the form tag into partial and added update contact feature

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   root 'contacts#index' # To specify the homepage
   
   get 'contacts/index', as: 'contacts' # to redirect contacts controller at index method
-  
   get 'contacts/new', as: 'new_contact' # to redirect to new contacts page
-  
-  post '/contacts/create', to: 'contacts#create'
+  post 'contacts/create', to: 'contacts#create', as: 'create_contact'
+  get 'contacts/:id/edit', to: 'contacts#edit', as: 'edit_contact'
+  patch 'contacts/:id/update', to: 'contacts#update', as: 'update_contact'
 end
