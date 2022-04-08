779ec6be402905874d568358804411e905ab5bf5
danieljpay <danielj.pay@hotmail.com>
2022-04-07 22:50:11 -0500

    Added delete contact feature

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//import "@hotwired/turbo-rails"
+import "@hotwired/turbo-rails"
 import "controllers"
 
 // Maybe here should be import instead of require:
 //= require jquery
+//= require jquery_ujs
 //= require bootstrap-sprockets
 //= require jasny-bootstrap.min
