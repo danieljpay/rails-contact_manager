d27f29fd3032495c8847e23137f0647f31765082
danieljpay <danielj.pay@hotmail.com>
2022-04-13 13:31:54 -0500

    AJAXified the pagination, table and search form

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,10 +3,6 @@ import "@hotwired/turbo-rails"
 import "controllers"
 
 // Maybe here should be import instead of require:
-//= require jquery
-//= require jquery_ujs
-//= require jquery-ui
 //= require bootstrap-sprockets
 //= require jasny-bootstrap.min
-//= require toastr
 //= require_tree .
